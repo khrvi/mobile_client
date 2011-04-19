f0fcf561c5b13d0da652003a40963906824f4f9d
Viacheslav Khrenov <khrviach@gmail.com>
2011-04-19 15:02:55 -0700

    added simple authentication system

--- client.rb
+++ client.rb
@@ -2,10 +2,55 @@ require 'rubygems'
 require 'sinatra'
 require 'haml'
 require './vendor/right_api_client/lib/right_api_client'
+enable :sessions
 
-get '/deployments' do
-  client = RightApiClient.new('email', 'password', 'account_id')
-  @deployment = client.deployments(:filters => ['name==khrvi_Rails_app']).first
-  haml :deployments
+helpers do
+  def authorized?
+    @current_client ||= login_from_session
+  end
+
+  def current_client
+    authorized?
+  end
+
+  def login_from_session
+    session[:client]
+  end
 end
 
+before do
+  # Skip before filter
+  if request.path_info == '/login'
+    redirect to('/') if authorized?
+  else
+    redirect to('/login') unless authorized?
+  end
+end
+
+get '/' do
+  redirect to('/deployments')
+end
+
+get '/login' do
+  haml :login
+end
+
+post '/login' do
+  if client = RightApiClient.new(params[:email], params[:password], params[:account])
+    puts client
+    session[:client] = client
+    redirect '/'
+  else
+    redirect '/login'
+  end
+end
+
+get '/logout' do
+  session.delete :client
+  redirect to('login')
+end
+
+get '/deployments' do
+  @deployment = current_client.deployments
+  haml :deployments
+end
